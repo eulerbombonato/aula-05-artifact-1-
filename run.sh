a673e10fa96ccb4ea02a5ad6d2e0eb2b43e36120
eulerbombonato <102261620+eulerbombonato@users.noreply.github.com>
2024-08-31 22:00:38 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
-chmod +x ./linux/hello-server
+#!/bin/bash
 
-./linux/hello-server &
+# Verifica se o arquivo existe antes de aplicar chmod
+if [ -f "./hello-server" ]; then
+    chmod +x ./hello-server
+    ./hello-server &
+    sleep 5
 
-sleep 5
-
-for LOGIN in Homer Bart Maggie;
-do
-    echo "$(date): $(curl -s http://localhost:13000/${LOGIN})"
-done
+    for LOGIN in Homer Bart Maggie; do
+        echo "$(date): $(curl -s http://localhost:13000/${LOGIN})"
+    done
+else
+    echo "Arquivo hello-server não encontrado!"
+    exit 1
+fi
